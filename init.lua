bab372a4b4bb7d1ac8da7babb3c4adbd8ea61e47
Juraj Petráš <jurkopetras@gmail.com>
2022-11-30 20:53:22 +0100

    Fixed copilot completion binding

--- init.lua
+++ init.lua
@@ -45,7 +45,9 @@ local config = {
             ["<leader>lk"] = { vim.lsp.buf.hover, desc = "Hover Documentation" },
         },
     },
-
 }
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", "copilot#Accept()", { silent = true, expr = true })
 
 return config
