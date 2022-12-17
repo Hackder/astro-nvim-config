303c3afe7c5917f67ccba73463a7a4f6711c5bc3
Juraj Petráš <jurkopetras@gmail.com>
2022-12-18 00:17:02 +0100

    Added markdown preview

--- init.lua
+++ init.lua
@@ -9,6 +9,13 @@ local config = {
   plugins = {
     init = {
       ["github/copilot.vim"] = {},
+      ["iamcco/markdown-preview.nvim"] = {
+        run = "cd app && yarn install",
+        ft = { "markdown" },
+        setup = function()
+          vim.g.mkdp_filetypes = { "markdown" }
+        end,
+      },
     },
     telescope = {
       defaults = {
